8539bf4ec50b69d3f9702d56fc775feb38045b86
shenzhiyongzhe <67083540+shenzhiyongzhe@users.noreply.github.com>
2024-01-25 11:13:31 +0800

    Add files via upload

---
+++ ChatApp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,59 @@
+﻿using ChatClient.Core;
+using ChatClient.MVVM.Model;
+using ChatClient.Net;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+
+namespace ChatClient.MVVM.ViewModel
+{
+    class MainViewModel
+    {
+        public ObservableCollection<UserModel> Users { get; set; }
+        public ObservableCollection<string> Messages { get; set; }
+        public RelayCommand ConnectToServerCommand {  get; set; }
+        public RelayCommand SendMessageCommand { get; set; }
+        public string UserName {  get; set; }
+        public string Message {  get; set; }
+
+        public Server _server;
+        public MainViewModel() 
+        {
+            Users = new ObservableCollection<UserModel>();
+            Messages = new ObservableCollection<string>();
+            _server = new Server();
+            _server.connectedEvent += UserConnected;
+            _server.msgReceivedEvent += MessageReceived;
+            _server.userDisconnectedEvent += RemoveUser;
+            ConnectToServerCommand = new RelayCommand(o => _server.ConnetToServer(UserName), o => !string.IsNullOrEmpty(UserName));
+            SendMessageCommand = new RelayCommand(o => _server.SendMessageToServer(Message), o => !string.IsNullOrEmpty(Message));
+        }
+
+        private void RemoveUser()
+        {
+            var uid = _server.packetReader.ReadMessage();
+            var user = Users.Where(x => x.UID == uid).FirstOrDefault();
+            Application.Current.Dispatcher.Invoke(() => { Users.Remove(user); });   
+        }
+
+        private void MessageReceived()
+        {
+            var msg = _server.packetReader.ReadMessage();
+            Application.Current.Dispatcher.Invoke(() => Messages.Add(msg));
+        }
+
+        private void UserConnected()
+        {
+            var user = new UserModel
+            {
+                UserName = _server.packetReader.ReadMessage(),
+                UID = _server.packetReader.ReadMessage(),
+            };
+            if(!Users.Any(x => x.UID == user.UID))
+            {
+                Application.Current.Dispatcher.Invoke(() => Users.Add(user));
+            }
+        }
+    }
+}
---
+++ ChatApp/Net/Server.cs
@@ -0,0 +1,70 @@
+﻿using ChatClient.Net.IO;
+using System;
+using System.Net.Sockets;
+
+
+namespace ChatClient.Net
+{
+    class Server
+    {
+        TcpClient _client;
+
+        public PacketReader packetReader;
+
+        public event Action connectedEvent;
+        public event Action msgReceivedEvent;
+        public event Action userDisconnectedEvent;
+
+        public Server()
+        {
+            _client = new TcpClient();
+        }
+
+        public void ConnetToServer(string username)
+        {
+            if(!_client.Connected)
+            {
+                _client.Connect("127.0.0.1", 7891);
+                packetReader = new PacketReader(_client.GetStream());
+                var connectPacket = new PacketBuilder();
+                connectPacket.WriteOpCode(0);
+                connectPacket.WriteMessage(username);
+                _client.Client.Send(connectPacket.GetPacketBytes());
+            }
+            ReadPackets();
+        }
+        private void ReadPackets()
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    var opcode = packetReader?.ReadByte();
+                    switch (opcode)
+                    {
+                        case 1:
+                            connectedEvent?.Invoke();
+                            break;
+                        case 5:
+                            msgReceivedEvent?.Invoke();
+                            break;
+                        case 10:
+                            userDisconnectedEvent?.Invoke();
+                            break;
+                        default:
+                            Console.WriteLine("oh yes...");
+                            break;
+                    }
+                }
+            });
+        }
+
+        public void SendMessageToServer(string message)
+        {
+            var messagePacket = new PacketBuilder();
+            messagePacket.WriteOpCode(5);
+            messagePacket.WriteMessage(message);
+            _client.Client.Send(messagePacket.GetPacketBytes());
+        }
+    }
+}
